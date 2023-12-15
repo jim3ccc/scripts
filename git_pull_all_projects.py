dce0b5ca5e95445e8c00afb86745530b6fe7430e
Jim <jimchiang001@gmail.com>
2023-12-15 07:53:25 -0800

    Create git_pull_all_projects.py

---
+++ git_pull_all_projects.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+
+def git_pull_in_directories(root_dir='.'):
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        if '.git' in dirnames:
+            git_directory = os.path.join(dirpath, '.git')
+            try:
+                subprocess.run(['git', 'pull'], cwd=dirpath, check=True)
+                print(f"Git pull successful in {dirpath}")
+            except subprocess.CalledProcessError as e:
+                print(f"Error in {dirpath}: {e}")
+        else:
+            continue
+
+if __name__ == "__main__":
+    root_directory = input("Enter the root directory (default is current directory): ").strip()
+    if not root_directory:
+        root_directory = '.'
+
+    git_pull_in_directories(root_directory)
